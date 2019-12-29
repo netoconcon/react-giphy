bc776affea0f2de759950773d5f125fa7ec56312
Neto Concon <concon.neto2@gmail.com>
2019-12-28 22:01:23 -0300

    layout

--- src/index.jsx
+++ src/index.jsx
@@ -1,42 +1,15 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import App from './components/app.jsx';
 
 import '../assets/stylesheets/application.scss';
 
 
 
-class Hello extends Component {
-  constructor(props) {
-    super(props);
 
-    this.state = {
-      clicked: false,
-      counter: 0
-    };
-  }
-
-  handleClick = () => {
-    this.setState({
-      clicked: !this.state.clicked,
-      counter: this.state.counter +1
-    });
-  }
-
-
-  render() {
-    return (
-      <div className={this.state.clicked ? 'clicked' : null }
-            onClick={this.handleClick} >
-
-        Hello {this.props.name} {this.state.counter}
-
-      </div>
-      )
-  }
-}
 
 
 const root = document.getElementById('root');
 if (root) {
-  ReactDOM.render(<Hello name="World" />, root);
+  ReactDOM.render(<App />, root);
 }
